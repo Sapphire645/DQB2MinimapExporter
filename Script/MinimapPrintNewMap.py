47373b9cb5e06eb54df46085a7da4ce721620d52
Sapphire645 <167467641+Sapphire645@users.noreply.github.com>
2024-09-03 13:04:39 +0200

    1.0.0

--- Script/MinimapPrintNewMap.py
+++ Script/MinimapPrintNewMap.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 import tkinter as tk
 from tkinter import filedialog
+import zlib
 
 ##Pointer
 Start = 2401803
@@ -15,7 +16,15 @@ map_chunky_path = 'SheetChunky.png'
 image_width = 256
 
 ##Crop sizes for Heresy mask
-HeresyDictionary = {0:[1152,1456,1728+1152,1456+1216]}
+HeresyDictionary = {0:[1152,1456,1728+1152,1456+1216],
+                    1:[320,816,2816+320,2048+816],
+                    2:[1024,1008,1920+1024,1664+1008],
+                    20:[1024,1008,1920+1024,1664+1008],
+                    21:[1024,1008,1920+1024,1664+1008],
+                    3:[832,304,2304+832,3072+304],
+                    4:[576,48,2112+576,2880+48],
+                    8:[1344,560,1728+1344,1856+560]
+                    }
 ##Tiles that can have mountains
 MountainList = (0x59,0xC7,0x6F,0x64,0x7A)
 
@@ -103,19 +112,20 @@ def construct_image(binary_data, tiles,mountain, image_width, HD):
         try:
             constructed_image = constructed_image.crop((HeresyDictionary[HD]))
         except:
-            print(f"Hermit's Heresy not supported yet! {HD}")
+            print(f"Hermit's Heresy cropping not supported yet! {HD}")
         if valid[4]:
             constructed_image = constructed_image.resize((constructed_image.size[0]//2, constructed_image.size[1]//2), Image.NEAREST)
         else:    
             constructed_image = constructed_image.resize((constructed_image.size[0]//2, constructed_image.size[1]//2), Image.LANCZOS)
     return constructed_image
 
-
 def process(binary_file_path,mountain_folder_path,image_width, islands,folder_path):
     # Load the binary data from the file
     with open(binary_file_path, 'rb') as f:
-        binary_data = list(f.read())
-
+        binary_data = f.read()
+    if valid[5]:
+        binary_data= zlib.decompress(binary_data[0x2A444:])
+    binary_data = list(binary_data)
     # Load the tiles from the folder
     if not valid[4]:
         tiles = extract_tiles_from_map(map_path)
@@ -145,16 +155,23 @@ Island_names = ((0,"Isle of\nAwakening\n\nからっぽ島","aqua"),(1,"Furrowfie
                 (2,"Khrumbul-Dun\nオッカムル島","orange"),(20,"Upper level","#C47400"),(21,"Lower level","#944D00"),
                 (3,"Moonbrooke\n\nムーンブルク島","#00FFC0"),(4,"Malhalla\n\n破壊天体シドー","#BBBBFF"),
                 (8,"Skelkatraz\n\n監獄島","grey"),(10,"Buildertopia 1\n\nかいたく島1","#FFDDDD"),(11,"Buildertopia 2\n\nかいたく島2","#FFE6DD"),
-                (13,"Buildertopia 3\n\nかいたく島3","#FFEEDD"),(7,"Angler's Isle\n\nツリル島","#DDF0FF"))
+                (13,"Buildertopia 3\n\nかいたく島3","#FFEEDD"),(7,"Angler's Isle\n\nツリル島","#DDF0FF"),(12,"Battle Atoll   バトル島","#FF9999"))
 
-valid = [False,"",True,False,0] #Python.... [Valid, Folder_Path, Visible, Hermit_Mask, chunky_configuration]
+valid = [False,"",True,False,0,False] #Python.... [Valid, Folder_Path, Visible, Hermit_Mask, chunky_configuration, cmndat]
 
+def Text_List_Lan():
+    with open("MenuText.txt", 'r',encoding='utf-8') as file:
+        lines = file.readlines()
+        Text_List = [line.strip() for line in lines]
+    return Text_List
+
+Text_List = Text_List_Lan()
 
 def export_check():
     #Checks requirements and sends the info to the export func.
     if valid[0] == True:
         folder_path = filedialog.askdirectory(
-            title="Select a Folder"
+            title= Text_List[0]#Select a Folder
         )
         if folder_path:
             Islands = []
@@ -163,49 +180,81 @@ def export_check():
                 if i.get() == 1:
                     Islands.append(Island_names[c][0])
                 c += 1
-            path_Check.config(text="Please wait...",fg = "orange")
+            path_Check.config(text=Text_List[1],fg = "orange") #Please wait...
             root.update()
             process(valid[1], mountain_folder_path, image_width, Islands,folder_path)
         
 def checkfile(file_path):
+    for i in range(13):
+        if button_vars[i].get() == -1:
+            button_vars[i].set(0)
     #Tries to see if the file is valid. Fails miserably.
     with open(file_path, 'rb') as f:
         binary_data = list(f.read())
     if list(binary_data[0:8]) == list([252, 255, 190, 62, 180, 255, 127, 192]):
-        path_Check.config(text="Valid file!",fg = "green")
+        path_Check.config(text=Text_List[2],fg = "green") #Valid file!
         valid[0] = True
+        valid[5] = False
     else:
         if len(binary_data) == 0x55DD3A:
-            path_Check.config(text="Valid file! (Steam save)",fg = "lime")
+            path_Check.config(text=f"{Text_List[2]} (Debug save failsafe)",fg = "lime") #Valid file! (Debug save failsafe)
             valid[0] = True
+            valid[5] = False
         else:
-            path_Check.config(text="Invalid file!",fg = "red")
-            valid[0] = False
+            if list(binary_data[0:4]) == list([0x61,0x65,0x72,0x43]):
+                path_Check.config(text=f"{Text_List[2]} (CMN.DAT)",fg = "green") #Valid file! (Debug save failsafe)
+                valid[5] = True
+                valid[0] = True
+            else:
+                valid[5] = False
+                path_Check.config(text=Text_List[3],fg = "red") #Invalid file!
+                valid[0] = False
+    if valid[0]: ##WELCOME TO SPAGUETTI CODE LAND
+        IslandsCheck = []
+        if valid[5]:
+            with open(file_path, 'rb') as f:
+                binary_data = f.read()
+                binary_data= zlib.decompress(binary_data[0x2A444:])
+                binary_data = list(binary_data)
+        c = 0
+        for i in button_vars:
+            IslandsCheck.append(Island_names[c][0])
+            c += 1
+        c = 0
+        CheckPointer = Start
+        for i in range(24):
+            if i in IslandsCheck:
+                if binary_data[CheckPointer] == 0:
+                    button_vars[IslandsCheck.index(i)].set(-1)
+            CheckPointer = CheckPointer + End + 4
+        
             
 def open_file_dialog():
     # Open a file dialog and allow the user to select a file
     file_path = filedialog.askopenfilename(
-        title="Select binary file (Turtle-Insect's exported CMN.DAT)",
+        title=Text_List[4], #Select binary file (Turtle-Insect's exported CMN.DAT)
         filetypes=[("All Files", "*.*")]
     )
     if file_path:
-        path_Label.config(text=f"Selected file: {file_path}")
+        path_Label.config(text=f"{Text_List[5]}:{file_path}") #Selected file:
         checkfile(file_path)
         valid[1] = file_path
     else:
-        path_Label.config(text="No file selected")
+        path_Label.config(text=Text_List[6]) #No file selected
 
 
 def on_button_click(index):
     #Island click
-    if button_vars[index].get() == 0:
-        button_vars[index].set(1)
-    else:
-        button_vars[index].set(0)
+    if button_vars[index].get() != -1:
+        if button_vars[index].get() == 0:
+            button_vars[index].set(1)
+        else:
+            button_vars[index].set(0)
 
 def select_command(upda):
     for var in button_vars:
-        var.set(upda)
+        if var.get() != -1:
+            var.set(upda)
 
 def visible_command():
     #Visible click
@@ -227,7 +276,6 @@ def heresy_command():
 
 def chunky_command(button):
     if button == valid[4]:
-        print(button)
         valid[4] = 0
         chunky_button_1x.config(bg = "SystemButtonFace")
         chunky_button_16x.config(bg = "SystemButtonFace")
@@ -243,7 +291,7 @@ def chunky_command(button):
 
 # Create the main window
 root = tk.Tk()
-root.title("Minimap Exporter")
+root.title(Text_List[7])
 root.iconbitmap('icon.ico')
 
 #No comments get lost future me 
@@ -253,7 +301,7 @@ buttons = []
 
 open_file_button = tk.Button(
     root,
-    text="Open File",
+    text=Text_List[8], #Open File
     command=open_file_dialog
 )
 
@@ -270,7 +318,7 @@ options_frame.pack(padx=0, pady=0)
 select_button = tk.Button(
     options_frame,
     width = 13,
-    text="Select All",
+    text=Text_List[9], #Select All
     command=lambda i=True:select_command(i)
 )
 
@@ -280,7 +328,7 @@ select_button.grid(row=0, column=0, padx=5, pady=0)
 unselect_button = tk.Button(
     options_frame,
     width = 13,
-    text="Unselect All",
+    text=Text_List[10], #Unselect All
     command=lambda i=False:select_command(i)
 )
 
@@ -289,7 +337,7 @@ unselect_button.grid(row=1, column=0, padx=5, pady=0)
 visible_button = tk.Button(
     options_frame,
     height = 3,
-    text="Hide unexplored\ntiles",
+    text=f"{Text_List[11]}\n{Text_List[12]}",#Hide unexplored\ntiles
     command=visible_command
 )
 
@@ -298,7 +346,7 @@ visible_button.grid(row=0, column=1,rowspan=2, padx=5, pady=5)
 heresy_button = tk.Button(
     options_frame,
     height = 3,
-    text="Hermit's Heresy\nmask",
+    text=f"{Text_List[13]}\n{Text_List[14]}", #Hermit's Heresy\nmask
     command=heresy_command
 )
 
@@ -307,7 +355,7 @@ heresy_button.grid(row=0, column=2,rowspan=2, padx=5, pady=5)
 chunky_button_1x = tk.Button(
     options_frame,
     width = 13,
-    text="Chunky Tiles 1x",
+    text=Text_List[15], #Chunky Tiles 1x
     command=lambda i=1:chunky_command(i),
     pady=0
 )
@@ -317,7 +365,7 @@ chunky_button_1x.grid(row=0, column=3, padx=5, pady=0)
 chunky_button_16x = tk.Button(
     options_frame,
     width = 13,
-    text="Chunky Tiles 16x",
+    text=Text_List[16], #Chunky Tiles 16x
     command=lambda i=16:chunky_command(i),
     pady=0
 )
@@ -328,13 +376,13 @@ button_frame = tk.Frame(root)
 button_frame.pack(padx=10, pady=10)
 
 export_button = tk.Button(
-    root,
+    button_frame,
     width = 30,
-    text="Export!",
+    text=Text_List[17], #Export!
     command=export_check
 )
 
-export_button.pack(pady=20)
+export_button.grid(row=6,column=1,columnspan=3,padx=0, pady=20)
 
 # Island buttons
 co = 0
@@ -364,13 +412,29 @@ for i in range(12):
         co += 1
     buttons.append(button)
 
+var = tk.IntVar()
+button_vars.append(var)
+
+battle_button = tk.Button(
+    button_frame,
+    width = button_size // 5,
+    text=Island_names[12][1], #Export!
+    command=lambda i=12: on_button_click(i)
+)
+
+battle_button.grid(row=6,column=4,padx=0, pady=20)
+buttons.append(battle_button)
+
 # Color buttons
 def update_button_colors():
     for i, button in enumerate(buttons):
         if button_vars[i].get() == 1:
             button.config(bg=Island_names[i][2])
         else:
-            button.config(bg='SystemButtonFace')
+            if button_vars[i].get() == 0:
+                button.config(bg='SystemButtonFace')
+            else:
+                button.config(bg='#999999')
 
 # "Attach the update_button_colors function to each IntVar" sure i believe u
 for var in button_vars:
